039e695aa685c3adbbcdb6ba7ae8760751b38a4d
sarika-ui <kumarisarika1144@gmail.com>
2025-08-18 12:38:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { 
-    unoptimized: true,
-    domains: ['images.pexels.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
   },
 };
 
